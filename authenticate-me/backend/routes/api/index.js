a935b193c7a93f1b848bfe64e66c80f39dfff7db
Rhys Previte <previterp88@gmail.com>
2020-11-22 12:30:07 -0500

    Phase 4 of authenticate me complete

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,4 +1,12 @@
 const router = require('express').Router(); 
+const sessionRouter = require('./session.js'); 
+const usersRouter = require('./users.js'); 
+
+router.use('/session', sessionRouter); 
+
+router.use('/users', usersRouter); 
+
+
 
 //TEST CASES 
 
@@ -8,30 +16,30 @@ const router = require('express').Router();
 // });
 
 //2: test token cookie works
-const asyncHandler = require('express-async-handler'); 
-const { setTokenCookie } = require('../../utils/auth.js'); 
-const { User } = require('../../db/models'); 
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo'
-        },
-    })
-    setTokenCookie(res, user); 
-    return res.json({ user }); 
-})); 
+// const asyncHandler = require('express-async-handler'); 
+// const { setTokenCookie } = require('../../utils/auth.js'); 
+// const { User } = require('../../db/models'); 
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo'
+//         },
+//     })
+//     setTokenCookie(res, user); 
+//     return res.json({ user }); 
+// })); 
 
 //3: test restoreUser auth 
-const {restoreUser} = require('../../utils/auth.js'); 
-router.get('/restore-user', restoreUser, (req, res) => {
-    return res.json(req.user); 
-});
+// const {restoreUser} = require('../../utils/auth.js'); 
+// router.get('/restore-user', restoreUser, (req, res) => {
+//     return res.json(req.user); 
+// });
 
 //4: test require auth 
-const { requireAuth } = require('../../utils/auth.js'); 
-router.get('/require-auth', requireAuth, (req, res) => {
-    return res.json(req.user); 
-});
+// const { requireAuth } = require('../../utils/auth.js'); 
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user); 
+// });
 
 
 
