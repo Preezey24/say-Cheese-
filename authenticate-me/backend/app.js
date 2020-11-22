6857d933c2aafe5cae70f210128fb6feb114ded8
Rhys Previte <previterp88@gmail.com>
2020-11-22 11:12:52 -0500

    Backend auth & User functions established

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -5,6 +5,8 @@ const csurf = require('csurf');
 const helmet = require('helmet'); 
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config'); 
+const routes = require('./routes'); 
+const { ValidationError } = require('sequelize'); 
 
 const isProduction = environment === 'production'; 
 const app = express(); 
@@ -19,8 +21,43 @@ app.use(cookieParser());
 app.use(express.json()); 
 app.use(
     csurf({
-        secure: isProduction, 
-        sameSite: isProduction && "Lax",
-        httpOnly: true, 
+        cookie: {
+            secure: isProduction, 
+            sameSite: isProduction && "Lax",
+            httpOnly: true, 
+        }
     })
-)
+); 
+app.use(routes); 
+
+//Catch unhandled requests and forward to error handler
+app.use((_req, _res, next) => {
+    const err = new Error('The requested resource could not be found'); 
+    err.title = 'Resource Not Found'; 
+    err.errors = ['The requested resource could not be found']; 
+    err.status = 404; 
+    next(err); 
+});
+
+//Sequelize error handler
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message); 
+        err.title = 'Validation error'; 
+    }
+    next(err); 
+});
+
+//Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500); 
+    console.error(err); 
+    res.json({
+        title: err.title || 'Server Error', 
+        message: err.message, 
+        errors: err.errors, 
+        stack: isProduction ? null : err.stack, 
+    });
+});
+
+module.exports = app; 
