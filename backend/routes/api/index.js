1d2deb22d08d0d8faaaca8605e064828541c7944
Rhys Previte <previterp88@gmail.com>
2020-11-23 12:34:01 -0500

    Complete file restructuring

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,47 +1,46 @@
-const router = require("express").Router();
-const sessionRouter = require("./session.js");
-const usersRouter = require("./users.js");
+const router = require('express').Router(); 
+const sessionRouter = require('./session.js'); 
+const usersRouter = require('./users.js'); 
 
-// // GET /api/set-token-cookie
-// const asyncHandler = require('express-async-handler');
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
+router.use('/session', sessionRouter); 
+
+router.use('/users', usersRouter); 
+
+
+
+//TEST CASES 
+
+//1: test routing response 
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body }); 
+// });
+
+//2: test token cookie works
+// const asyncHandler = require('express-async-handler'); 
+// const { setTokenCookie } = require('../../utils/auth.js'); 
+// const { User } = require('../../db/models'); 
 // router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         username: 'Demo-lition'
-//       },
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo'
+//         },
 //     })
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }));
+//     setTokenCookie(res, user); 
+//     return res.json({ user }); 
+// })); 
 
-// // GET /api/restore-user
-// const { restoreUser } = require('../../utils/auth.js');
-// router.get(
-//   '/restore-user',
-//   restoreUser,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+//3: test restoreUser auth 
+// const {restoreUser} = require('../../utils/auth.js'); 
+// router.get('/restore-user', restoreUser, (req, res) => {
+//     return res.json(req.user); 
+// });
 
-// // GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+//4: test require auth 
+// const { requireAuth } = require('../../utils/auth.js'); 
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user); 
+// });
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
 
-router.use("/session", sessionRouter);
 
-router.use("/users", usersRouter);
-
-module.exports = router;
+module.exports = router; 
