4ef6382291ecd15bd3c65b810ed5f2eb1dedfa9a
Rhys Previte <previterp88@gmail.com>
2021-02-09 17:33:22 -0500

    Testing heroku deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux'; 
+import { useDispatch, useSelector } from 'react-redux'; 
 import { Route, Switch, useLocation } from 'react-router-dom'; 
 import SignUpFormPage from './components/SignUpFormPage'; 
 import * as sessionActions from './store/session'; 
 import * as photoActions from './store/photo'; 
-import Navigation from "./components/Navigation"; 
+import Navigation from "./components/Navigation";
+import SplashPage from "./components/SplashPage";  
 import HomePage from './components/HomePage';
 import PhotoPage from './components/PhotoPage';
 import SearchResults from './components/PhotoPage/SearchResults'; 
@@ -12,6 +13,7 @@ import SearchResults from './components/PhotoPage/SearchResults';
 function App() {
   const dispatch = useDispatch(); 
   const [isLoaded, setIsLoaded] = useState(false); 
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
   const [tag, setTag] = useState(''); 
   const location = useLocation();
   useEffect(() => {
@@ -30,7 +32,7 @@ function App() {
   return (
     <>
     <Navigation isLoaded={isLoaded} />
-    {isLoaded && (
+    {isLoaded && isAuthenticated && (
       <Switch>
         <Route exact path="/">
           <HomePage/>
@@ -45,6 +47,11 @@ function App() {
           <SearchResults /> 
         </Route>
       </Switch>
+    )}
+    {!isAuthenticated && (
+      <Route exact path="/">
+        <SplashPage/>
+      </Route>
     )}
   </>
   );
---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import Footer from "./Footer"
+import "./SplashPage.css"
+
+const SplashPage = () => {
+    return (
+        <>
+            <Footer/>
+        </>
+    )
+}
+
+export default SplashPage;
